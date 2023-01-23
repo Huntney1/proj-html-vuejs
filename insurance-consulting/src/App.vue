4036122130f22da1b1397aab0c6667cb55495832
Huntney1 <devilwhiteone@hotmail.it>
2023-01-23 16:50:07 +0100

    preparazione lavoro

---
+++ insurance-consulting/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+};
+</script>
+
+<template>
+  <div>
+   
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
